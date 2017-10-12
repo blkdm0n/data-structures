0bc9e544e43e4ebed65091f5be68ccf7170ed1dd
aikerim <aygerim.sauletkhan@gmail.com>
2017-10-12 09:51:53 -0700

    functional shared and prototype

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+	var storage = {}
+	var key = 0
+	var start = 0
 };
 
+Queue.prototype.enqueue = function(val){ 
+	console.log(this.storage)
+	this.storage[this.key] = val 
+	this.key++
+}
 
+Queue.prototype.dequeue = function(){ 
+	var first = this.storage[this.start]
+	if(first){
+		delete this.storage[this.start]
+		this.start++
+	}
+	return first
+}
+
+Queue.prototype.size = function(val){ 
+	return this.key - this.start
+}
+
+var q1 = new Queue()
+q1.enqueue(1)
