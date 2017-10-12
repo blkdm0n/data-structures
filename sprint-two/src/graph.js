eb04c3b658d5659f2b64d141e1d16c284ebe4d38
blkdm0n <bernard.kitchens@gmail.com>
2017-10-12 16:40:28 -0700

    partially completed tree.js

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,18 +2,22 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,18 @@
+var extend = function(destination, source) { 
+  for (var key in source) {
+    destination[key] = source[key]; 
+  }
+};
+
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+  
+  
+  extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,13 +20,44 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var childTree = Tree(value);
+  this.children.push(childTree);
 };
 
 treeMethods.contains = function(target) {
+  var root = this;
+  console.log(root);
+  var traverser = function(node) {
+    if (node.value === target) {
+      return true; 
+      
+    } else {
+      
+      node.children.forEach(function(subTree) {
+        traverser(subTree);
+      });
+      
+      return false; 
+    }
+  };
+  return traverser(root);
+  // return false;
 };
+
+// var example = Tree(1);
+// example.addChild(2);
+// example.addChild(3);
+// example.addChild(4);
+// example.children[0].addChild(5);
+// console.log(example.contains(55))
+
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+// .children property, an array containing a number of subtrees
+// .addChild() method, takes any value, sets that as the target of a node, and adds that node as a child of the tree
+// A .contains() method, takes any input and returns a boolean reflecting whether it can be found as the value of the target node or any descendant node
+// What is the time complexity of the above functions?
